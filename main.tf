e13780742625a2db13b0a2e56c0e58932a3fe128
Its for Devops <itsfordevops123@gmail.com>
2020-02-10 20:36:07 -0800

    terraform initialization

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 provider "aws" {
   region= "us-east-2"
+  {
+   assume_role{
+       role_arn = "arn:aws:iam::486674086191:user/hema"
+       session_name = "SESSION_NAME"
+       external_id  = "EXTERNAL_ID"
+
+   }   
+  }
 }
 resource "aws_instance" "HemaEc2" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type="t2.micro"
   
+}
+tags {
+    name= "HemaEC2"
 }
