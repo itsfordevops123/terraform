4aca377211f9af6928fdba54557191c6a778f120
Its for Devops <itsfordevops123@gmail.com>
2020-02-10 20:44:57 -0800

    terraform initialization

--- main.tf
+++ main.tf
@@ -10,8 +10,7 @@ provider "aws" {
 resource "aws_instance" "HemaEc2" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type="t2.micro"
-  
-}
-tags = {
-    name= "HemaEC2"
+    tags = {
+        name= "HemaEC2"
+    }    
 }
